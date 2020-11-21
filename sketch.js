a21af9c4c53bd3d2f2a0b3009de1509fd2aaa334
AaravBeast123 <71824389+AaravBeast123@users.noreply.github.com>
2020-11-21 19:59:03 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,3 +1,18 @@
+const Engine = Matter.Engine;
+const World = Matter.World;
+const Bodies = Matter.Bodies;
+const Body = Matter.Body;
+const Render = Matter.Render;
+
+var dustbinObj, paperObject,groundObject	
+var world;
+
+function preload()
+{
+	
+}
+
+function setup() 
 class paper
 {
 	constructor (x,y,r)
@@ -32,35 +47,30 @@ display()
 
 }
 }
-const Engine = Matter.Engine;
-const World = Matter.World;
-const Bodies = Matter.Bodies;
-const Body = Matter.Body;
 
-function preload()
+
 {
-	
-}
-
-function keypressed()
-{
-	if (keyCode === UP_ARROW) {
-
-		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
-	}
-}
-function setup() {
-	createCanvas(1600, 400);
-
+	createCanvas(1600, 700);
+rectMode(CENTER)
 
 	engine = Engine.create();
 	world = engine.world;
+	dustbinObj=new dustbin(1200,650);
+	paperObject=new paper(200,450,40);
+	groundObject=new ground(width/2,670,width,20);
 
-	//Create the Bodies Here.
-
-
-	Engine.run(engine);
+	var render = Render.create({
+		element: document.body,
+		engine: engine,
+		options: {
+		  width: 1200,
+		  height: 700,
+		  wireframes: false
+		}
+	  });
   
+	  Engine.run(engine);
+	
 }
 
 
@@ -68,9 +78,23 @@ function draw() {
   rectMode(CENTER);
   background(0);
   
+  dustbinObj.display();
+  paperObject.display();
+  groundObject.display();
+
+}
   drawSprites();
  
+  function keypressed()
+{
+	if (keyCode === UP_ARROW) {
+
+		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+	}
 }
+
+
+
 
 
 
