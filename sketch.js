136406dc948ed9228cc4b1c58eee88f00949af79
AaravBeast123 <71824389+AaravBeast123@users.noreply.github.com>
2020-11-22 10:47:34 +0530

    Add files via upload

--- sketch.js
+++ sketch.js
@@ -1,98 +1,63 @@
+
 const Engine = Matter.Engine;
 const World = Matter.World;
 const Bodies = Matter.Bodies;
 const Body = Matter.Body;
 const Render = Matter.Render;
-
 var dustbinObj, paperObject,groundObject	
 var world;
 
-function preload()
-{
-	
-}
 
-function setup() 
-class paper
-{
-	constructor (x,y,r)
-{
-	var options={
-		isStatic:false,
-		restitution:0.3,
-		friction:0.5,
-		density:1.2,
-
-
-	}
-	this.x=x;
-	this.y=y;
-	this.r=r;
-	this.body=Bodies.circle(this.x, this.y, this.r/2, options)
-	World.add(world, this.body);
-
-}
-display()
-{
-
-	var paperpos=this.body.position;
-
-	push()
-	translate(paperpos.x, paperpos.y);
-	rectMode(CENTER)
-	strokeWeight(3);
-	fill(255,0,255)
-	ellipse(0,0,this.r, this.r);
-	pop()
-
-}
-}
-
-
-{
+function setup() {
 	createCanvas(1600, 700);
-rectMode(CENTER)
+	rectMode(CENTER);
+
 
 	engine = Engine.create();
 	world = engine.world;
 	dustbinObj=new dustbin(1200,650);
 	paperObject=new paper(200,450,40);
 	groundObject=new ground(width/2,670,width,20);
+	//Create a Ground
+	
 
 	var render = Render.create({
-		element: document.body,
-		engine: engine,
-		options: {
-		  width: 1200,
-		  height: 700,
-		  wireframes: false
-		}
-	  });
+	  element: document.body,
+	  engine: engine,
+	  options: {
+	    width: 1200,
+	    height: 700,
+	    wireframes: false
+	  }
+	});
+
+	Engine.run(engine);
+	//Render.run(render);
   
-	  Engine.run(engine);
-	
 }
 
 
 function draw() {
   rectMode(CENTER);
   background(0);
-  
+ 
   dustbinObj.display();
   paperObject.display();
   groundObject.display();
-
-}
-  drawSprites();
+  
  
-  function keypressed()
-{
-	if (keyCode === UP_ARROW) {
-
-		Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
-	}
+  
+  
+ 
 }
 
+function keyPressed() {
+  	if (keyCode === UP_ARROW) {
+
+    	Matter.Body.applyForce(paperObject.body,paperObject.body.position,{x:85,y:-85});
+    
+  	}
+}
 
 
 
